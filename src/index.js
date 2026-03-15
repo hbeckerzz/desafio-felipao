c79e7672c2b4b2cffc0058c24d21c56c270fb5b8
hbeckerzz <henriquescbecker@gmail.com>
2026-03-15 20:16:04 -0300

    projeto felipao

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("oi");
+let nome_do_heroi = "Leticinha";
+let quantidade_de_experiencia = "7000";
+
+
+if (quantidade_de_experiencia < 1000) {
+    let nivel_do_heroi = "Ferro";
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 1001 && quantidade_de_experiencia <= 2000) {
+    let nivel_do_heroi = "Bronze"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 2001 && quantidade_de_experiencia <= 5000) {
+    let nivel_do_heroi = "Prata"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 5001 && quantidade_de_experiencia <= 7000) {
+    let nivel_do_heroi = "Ouro"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 7001 && quantidade_de_experiencia <= 8000) {
+    let nivel_do_heroi = "Platina"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 8001 && quantidade_de_experiencia <= 9000) {
+    let nivel_do_heroi = "Ascendente"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else if (quantidade_de_experiencia >= 9001 && quantidade_de_experiencia <= 10000) {
+    let nivel_do_heroi = "Imortal"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+} else {
+    let nivel_do_heroi = "Radiante"
+    console.log("O Herói de nome " + nome_do_heroi + " está no nível de " + nivel_do_heroi);
+}
